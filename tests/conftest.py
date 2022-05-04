14e46374324138b1cf715083d727ae1ed7596b9c
lluar <lluar@ccodein.org>
2022-05-04 18:39:25 +0200

    ADD: pytest cli option epson-ip

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,21 @@ import pytest
 from tse.backends.epson import TSE as EpsonTSE
 
 
+def pytest_addoption(parser):
+    """Add pytest command line option."""
+    parser.addoption(
+        '--epson_ip', action='store', default='10.0.0.2',
+        help='The IP address of the Epson TSE.'
+    )
+
+
 @pytest.fixture(params=[pytest.param(EpsonTSE, marks=pytest.mark.epson)])
-def tse_backend(request):
+def tse(request, pytestconfig):
     """
     Yield multiple TSE backends.
 
     This fixture can be used to run a test for different
     backend.
     """
-    yield request.param
+    if request.param == EpsonTSE:
+        yield request.param(pytestconfig.getoption('epson_ip'))
