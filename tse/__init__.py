6cfb91d266482bc9ec2de6db6285c7ef27904ae7
lluar <lluar@ccodein.org>
2022-05-12 15:04:19 +0200

    ADD: TSEInfo dataclass

--- tse/__init__.py
+++ tse/__init__.py
@@ -1,5 +1,86 @@
 """The tse package."""
 from typing import Protocol
+from dataclasses import dataclass
+from enum import Enum, auto
+from datetime import datetime
+
+
+class TSEState(Enum):
+    """The state of the TSE."""
+
+    INITIALIZED = auto()
+    UNINITIALIZED = auto()
+    DECOMMISSIONED = auto()
+
+
+@dataclass(frozen=True)
+class TSEInfo:
+    """The class to access TSE infomation."""
+
+    public_key: str
+    """
+    Get the public key that belongs to the private key
+    generating signatures.
+    """
+
+    model_name: str
+    """Get TSE model name."""
+
+    state: TSEState
+    """Get initialization status of the TSE."""
+
+    has_valid_time: bool
+    """Has valid time is set in TSE."""
+
+    certificate_id: str
+    """Get certification ID as assigned by BSI."""
+
+    certificate_expiration_date: datetime
+    """Date after which the certificate of this TSE will be invalid. The
+    TSE will not be usable afterwards, all data must have been
+    exported before this date.
+    """
+
+    unique_id: str
+    """Get an identifier guaranteed to be unambiguous for every TSE."""
+
+    serial_number: str
+    """
+    A serial number is a hash value of a public key that belongs to a
+    key pair.
+    """
+
+    signature_algorithm: str
+    """The signature algorithm used by the TSE."""
+
+    signature_counter: int
+    """Amount of signatures that have been created with this TSE."""
+
+    remaining_signatures: int
+    """Remaining amount of signatures."""
+
+    max_signatures: int
+    """Remaining amount of signatures."""
+
+    registered_clients: int
+    """The number of currently registered clients."""
+
+    max_registered_clients: int
+    """Maximum number of clients that can be registered."""
+
+    max_started_transactions: int
+    """
+    The maximal number of simultaneously opened transactions
+    that can be managed by the TSE.
+    """
+    tar_export_size: int
+    """Size of the whole TSE store in bytes, if exported."""
+
+    needs_self_test: bool
+    """The TSE needs a self test."""
+
+    api_version: str
+    """The TSE's software version"""
 
 
 class TSE(Protocol):
