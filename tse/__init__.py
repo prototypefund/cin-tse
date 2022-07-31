99bcaae45d7c7f39c0db63741734c6b69d1c821f
lluar <lluar@ccodein.org>
2022-07-31 13:30:54 +0200

    ADD: TSE Protocol class

--- tse/__init__.py
+++ tse/__init__.py
@@ -1,5 +1,5 @@
 """The tse package."""
-from typing import Protocol
+from typing import Protocol, List
 from dataclasses import dataclass
 from enum import Enum, auto
 from datetime import datetime
@@ -125,4 +125,67 @@ class TSETransaction:
 
 
 class TSE(Protocol):
-    """The TSE protocol definition."""
+    def info(self) -> TSEInfo:
+        ...
+
+    def open(self) -> None:
+        ...
+
+    def close(self) -> None:
+        ...
+
+    def initialize(
+            self, puk: str, admin_pin: str, time_admin_pin: str) -> None:
+        ...
+
+    def login_user(
+            self, user_id: str, role: TSERole, pin: str) -> None:
+        ...
+
+    def logout_user(
+            self, user_id: str, role: TSERole) -> None:
+        ...
+
+    def change_pin(self, role: TSERole, puk: str, new_pin: str) -> None:
+        ...
+
+    def register_user(self, user_id: str) -> None:
+        ...
+
+    def deregister_user(self, user_id: str) -> None:
+        ...
+
+    def user_list(self) -> List[str]:
+        ...
+
+    def run_self_test(self) -> None:
+        ...
+
+    def factory_reset(self) -> None:
+        ...
+
+    def update_time(self, user_id: str, time: datetime) -> None:
+        ...
+
+    def lock(self, state: bool) -> None:
+        ...
+
+    def disable_secure_element(self) -> None:
+        ...
+
+    def start_transaction(
+            self, user_id: str, data: str, type: str) -> TSETransaction:
+        ...
+
+    def update_transaction(
+            self, user_id: str, transaction: TSETransaction,
+            data: str, type: str) -> None:
+        ...
+
+    def finish_transaction(
+            self, user_id: str, transaction: TSETransaction,
+            data: str, type: str) -> None:
+        ...
+
+    def started_transaction_list(self, user_id: str) -> List[int]:
+        ...
