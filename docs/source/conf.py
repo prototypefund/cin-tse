14aa76b31c32764ab7420fd266254ccfdd127987
lluar <lluar@ccodein.org>
2022-08-16 12:56:46 +0200

    ADD: version to 1.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, lluar'
 author = 'lluar'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
